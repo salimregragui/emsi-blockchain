6cb341982a3de8918c48b461227b3260c2a0db36
salimregragui <salim.regragui27@gmail.com>
2021-04-13 14:45:57 +0000

    ec_create betty

--- crypto/ec_create.c
+++ crypto/ec_create.c
@@ -1,7 +1,7 @@
 #include "hblk_crypto.h"
 
 /***
- * function that returns a pointer to an EC_KEY structure, 
+ * function that returns a pointer to an EC_KEY structure,
  * containing both the public and private keys, or NULL upon failure
  * Both the private and the public keys are generated
  * use of the secp256k1 elliptic curve to create the new pair
@@ -9,15 +9,15 @@
 
 EC_KEY *ec_create(void)
 {
-    EC_KEY *key;
+EC_KEY *key;
 
-    key = EC_KEY_new_by_curve_name(EC_CURVE);
-    if (!key)
-        return (NULL);
-    if (EC_KEY_generate_key(key) != 1)
-    {
-        EC_KEY_free(key);
-        return (NULL);
-    }
-    return (key);
+key = EC_KEY_new_by_curve_name(EC_CURVE);
+if (!key)
+return (NULL);
+if (EC_KEY_generate_key(key) != 1)
+{
+EC_KEY_free(key);
+return (NULL);
+}
+return (key);
 }
