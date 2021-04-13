e51798256c3a17214bf3b224ed5386333af73b64
salimregragui <salim.regragui27@gmail.com>
2021-04-13 15:03:14 +0000

    betty-doc fix

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -1,13 +1,22 @@
 #include "hblk_crypto.h"
 
 /**
- * function that extracts the public key from an EC_KEY
+ * ec_to_pub - extract the public key from an EC_KEY structure
+ *
+ * @key: pointer to the EC_KEY structure from which to extract the public key
+ * @pub: address at which to store the extracted public key
+ *
+ * Description: If @key is NULL, nothing will be done.
+ *
+ * Return: If @key is NULL or an error occurs, return NULL.
+ * Otherwise, return @pub.* function that extracts the public key from an EC_KEY
  *opaque structure
- *key is a pointer to the EC_KEY structure to retrieve
+ *@key: is a pointer to the EC_KEY structure to retrieve
  *the public key from. If it is NULL, your function must do nothing
  *and fail.
- *pub is the address at which to store the extracted
+ *@pub is the address at which to store the extracted
  *public key (The key shall not be compressed)
+ *Description : if @key is n
  */
 
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
