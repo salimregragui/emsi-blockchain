1a9f8241fa736edb7f4d957234f069292609b159
Salim Regragui <40745001+salimregragui@users.noreply.github.com>
2021-04-17 19:58:37 +0000

    ec_load 3

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,40 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
+
 #include "hblk_crypto.h"
 
-/**
- * ec_load - loads an existing EC key pair on the disk
- *
- * @folder: path to the folder from which to load the keys
- *
- * Return: pointer to EC key pair, or NULL on failure
- */
-EC_KEY *ec_load(char const *folder)
-{
-	EC_KEY *key = NULL;
-	char buffer[BUFSIZ];
-	FILE *fp;
 
-	if (folder == NULL)
-		return (NULL);
-
-	sprintf(buffer, "%s/%s", folder, PUB_FILENAME);
-	fp = fopen(buffer, "r");
-	if (!fp)
-		return (NULL);
-	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
+EC_KEY *ec_load(char const *folder){
+	
+	EC_KEY *key;
+	char pub_path[PATH_MAX];
+	char pv_path[PATH_MAX];
+	FILE *pubf;
+	FILE *pvf;
+	
+	if(!opendir(folder))
 	{
-		fclose(fp);
-		return (key);
+		fprintf(stderr,"%s","Unknown directory");	
 	}
-	sprintf(buffer, "%s/%s", folder, PRI_FILENAME);
-	fp = fopen(buffer, "r");
-	if (fp == NULL)
-		return (NULL);
-	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-	{
-		fclose(fp);
-		return (key);
-	}
+	snprintf(pub_path,PATH_MAX,"%s/%s",folder,PUB_FILENAME);
+	snprintf(pv_path,PATH_MAX,"%s/%s",folder,PRI_FILENAME);
 
-	fclose(fp);
-	return (key);
+	pubf = fopen(pub_path,"w+");
+	pvf = fopen(pv_path,"w+");
+
+	if(PEM_read_EC_PUBKEY(pubf,&key,NULL,NULL)==0){
+		fclose(pubf);
+		fclose(pvf);
+		return NULL;
+	}
+	if(PEM_read_ECPrivateKey(pvf,&key,NULL,NULL) == 0){
+		fclose(pubf);
+		fclose(pvf);
+		return NULL;
+	}
+	fclose(pubf);
+	fclose(pvf);
+	return key;
 }
