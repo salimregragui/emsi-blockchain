7341c662e907f0ddc55d27d2b636c974b3a18a65
Salim Regragui <40745001+salimregragui@users.noreply.github.com>
2021-04-17 19:57:00 +0000

    ec_load 2

--- crypto/ec_load.c
+++ crypto/ec_load.c
@@ -1,34 +1,40 @@
 #include "hblk_crypto.h"
 
 /**
-* ec_load - load keys from a file
-* @folder: folder where key pair is stored
-* Return: EC_KEY key pair
+ * ec_load - loads an existing EC key pair on the disk
+ *
+ * @folder: path to the folder from which to load the keys
+ *
+ * Return: pointer to EC key pair, or NULL on failure
  */
 EC_KEY *ec_load(char const *folder)
 {
 	EC_KEY *key = NULL;
-	char file[512] = {0};
+	char buffer[BUFSIZ];
 	FILE *fp;
-	struct stat st;
 
-	if (!folder)
+	if (folder == NULL)
 		return (NULL);
-	if (stat(folder, &st) == -1)
-		return (NULL);
-	sprintf(file, "./%s/%s", folder, PUB_FILENAME);
-	fp = fopen(file, "r");
+
+	sprintf(buffer, "%s/%s", folder, PUB_FILENAME);
+	fp = fopen(buffer, "r");
 	if (!fp)
 		return (NULL);
 	if (!PEM_read_EC_PUBKEY(fp, &key, NULL, NULL))
-		return (NULL);
-	fclose(fp);
-	sprintf(file, "./%s/%s", folder, PRI_FILENAME);
-	fp = fopen(file, "r");
-	if (!fp)
+	{
+		fclose(fp);
+		return (key);
+	}
+	sprintf(buffer, "%s/%s", folder, PRI_FILENAME);
+	fp = fopen(buffer, "r");
+	if (fp == NULL)
 		return (NULL);
 	if (!PEM_read_ECPrivateKey(fp, &key, NULL, NULL))
-		return (NULL);
+	{
+		fclose(fp);
+		return (key);
+	}
+
 	fclose(fp);
 	return (key);
 }
