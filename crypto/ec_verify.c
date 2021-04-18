e62a2366d0db078d08e5bca323b436fb11ab1f34
salimregragui <salim.regragui27@gmail.com>
2021-04-18 22:48:32 +0000

    ec_verify

--- crypto/ec_verify.c
+++ crypto/ec_verify.c
@@ -2,23 +2,18 @@
 #include "hblk_crypto.h"
 
 /**
- * ec_verify - verify a signature using an EC public key
- *
- * @key: pointer to the EC key pair containing the public key to be used
- * @msg: message to be signed
- * @msglen: length of @message
- * @sig: address at which to store the signature
- *
- * Return: If key, msg, or sig is NULL, or if verification fails, return 0.
- * Otherwise, return 1.
+ * ec_verify - verifies a message with public key
+ * @key: pointer to struct containing key pair
+ * @msg: the message to sign
+ * @msglen: length of message
+ * @sig: address to store signature
+ * Return: 1 if verified else 0
  */
-int ec_verify(
-	EC_KEY const *key, uint8_t const *msg, size_t msglen, sig_t const *sig)
+int ec_verify(EC_KEY const *key, uint8_t const *msg, size_t msglen,
+			  sig_t const *sig)
 {
-	if (key && msg && sig)
-	{
-		return (ECDSA_verify(
-			0, msg, msglen, sig->sig, sig->len, (EC_KEY *)key) == 1);
-	}
-	return (0);
+	if (!key || !msg || !sig)
+		return (0);
+	return (ECDSA_verify(0, msg, msglen, sig->sig, sig->len,
+						 (EC_KEY *)key) == 1);
 }
