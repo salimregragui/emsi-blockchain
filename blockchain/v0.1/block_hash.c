d4d96d394f657062418660c58eb78e6fb57e1c61
salimregragui <salim.regragui27@gmail.com>
2021-04-18 22:58:11 +0000

    blockchain

--- blockchain/v0.1/block_hash.c
+++ blockchain/v0.1/block_hash.c
@@ -1,16 +1,26 @@
 #include "blockchain.h"
 
 /**
-* block_hash - get the hash of a block
-* @block: block to be hashed, only hash field will not be hashed
-* @hash_buf: buffer for digest
-* Return: hash digest or NULL if failed
-*/
-uint8_t *block_hash(block_t const *block,
-					uint8_t hash_buf[SHA256_DIGEST_LENGTH])
+ * block_hash - compute the hash of a block
+ *
+ * @block: pointer to the block to be hashed
+ * @hash_buf: buffer in which to store the hash of @block
+ *
+ * Description: If @block is NULL or @hash_buf is NULL, do nothing.
+ * Otherwise, compute the hash of the memory area pointed to by @block.
+ *
+ * Return: If @block is NULL, return NULL.
+ * Otherwise, return @hash_buf.
+ */
+uint8_t *block_hash(
+	block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH])
 {
-	size_t len;
+	size_t len = 0;
 
-	len = sizeof(block->info) + block->data.len;
-	return (sha256((int8_t const *)block, len, hash_buf));
+	if (block && hash_buf)
+	{
+		len = sizeof(block->info) + block->data.len;
+		return (sha256((int8_t const *)block, len, hash_buf));
+	}
+	return (NULL);
 }
--- blockchain/v0.1/blockchain_serialize.c
+++ blockchain/v0.1/blockchain_serialize.c
@@ -1,56 +1,57 @@
 #include "blockchain.h"
 
 /**
-* write_block - function which writes a single block into @arg
-* @ptr: pointer to a block
-* @idx: index of a block
-* @arg: file pointer
-* Return: 0 if successful, -1 if failed
-*/
-int write_block(llist_node_t ptr, unsigned int idx, void *arg)
-{
-	FILE *fp;
-	block_t *block = ptr;
+ * blockchain_serialize - serialize a blockchain to a file
+ *
+ * @blockchain: pointer to the blockchain to be serialized
+ * @path: path to a file in which to write the serialized blockchain
+ *
+ * Description: If @path points to an existing file, it will be overwritten.
+ *
+ * Return: If an error occurs, return -1.
+ * Otherwise, return 0.
+ */
 
-	UNUSED(idx);
-	if (!arg || !ptr)
-		return (-1);
-	fp = (FILE *)arg;
-	fwrite((void *)&block->info, sizeof(block->info), 1, fp);
-	fwrite((void *)&block->data.len, sizeof(block->data.len), 1, fp);
-	fwrite(block->data.buffer, block->data.len, 1, fp);
-	fwrite(block->hash, sizeof(block->hash), 1, fp);
-	return (0);
-}
-
-/**
-* blockchain_serialize - function which writes a blockchain into a file
-* @blockchain: blockchain to be serialized
-* @path: path to the a file
-* Return: 0 if successful, -1 if failed
-*/
 int blockchain_serialize(blockchain_t const *blockchain, char const *path)
 {
-	hblk_file_t header;
-	FILE *fp;
+	int fd = -1;
+	int index = 0;
+	int32_t size = 0;
+	uint8_t endianness = _get_endianness();
+	block_t *block = NULL;
 
-	if (!blockchain || !path)
+	if (!blockchain || !blockchain->chain || !path)
 		return (-1);
-	memcpy(header.hblk_magic, "HBLK", 4);
-	memcpy(header.hblk_version, "0.1", 3);
-	header.hblk_endian = _get_endianness();
-	header.hblk_blocks = llist_size(blockchain->chain);
-	if (header.hblk_blocks == -1)
+	size = llist_size(blockchain->chain);
+	fd = open(path, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
+	if (fd == -1)
 		return (-1);
-	fp = fopen(path, "w");
-	if (!fp)
-		return (-1);
-	fwrite(&header, sizeof(header), 1, fp);
-	if (llist_for_each(blockchain->chain, write_block, fp) == -1)
+	if (write(fd, HBLK_MAGIC, HBLK_MAGIC_LEN) != HBLK_MAGIC_LEN)
+		return (close(fd), -1);
+	if (write(fd, HBLK_VERSION, HBLK_VERSION_LEN) != HBLK_VERSION_LEN)
+		return (close(fd), -1);
+	if (write(fd, &endianness, 1) != 1)
+		return (close(fd), -1);
+	if (write(fd, &size, 4) != 4)
+		return (close(fd), -1);
+
+	while (index < size)
 	{
-		fclose(fp);
-		return (-1);
+		block = llist_get_node_at(blockchain->chain, index++);
+		if (!block)
+			return (close(fd), -1);
+		if (write(fd, &(block->info), sizeof(block->info)) !=
+				sizeof(block->info))
+			return (close(fd), -1);
+		if (write(fd, &(block->data.len), 4) !=
+				4)
+			return (close(fd), -1);
+		if (write(fd, block->data.buffer, block->data.len) !=
+				block->data.len)
+			return (close(fd), -1);
+		if (write(fd, block->hash, SHA256_DIGEST_LENGTH) !=
+				SHA256_DIGEST_LENGTH)
+			return (close(fd), -1);
 	}
-	fclose(fp);
-	return (0);
+	return (close(fd), 0);
 }
